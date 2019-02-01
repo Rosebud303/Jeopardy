6e8dd84ca01e6f44592207531737c67a5b9030d3
Sally Haefling <sallyhaefling@gmail.com>
2019-01-31 18:26:01 -0700

    Add roundCreator method inside of roundChecker class

--- src/round-checker.js
+++ src/round-checker.js
@@ -8,12 +8,27 @@ class RoundChecker {
   createCategories() {
    this.categories = Object.keys(data.categories)
    for (let i = this.categories.length - 1; i > 0; i--) {
-       const j = Math.floor(Math.random() * (i + 1));
-       [this.categories[i], this.categories[j]] = [this.categories[j], this.categories[i]];  
-    }
+     const j = Math.floor(Math.random() * (i + 1));
+     [this.categories[i], this.categories[j]] = [this.categories[j], this.categories[i]];  
+   }
+ }
 
+ roundCreator() {
+  let categoryList = Object.keys(data.categories)
+  let categoryTitles = []
+  for(var i = 0; i < 4; i++){
+    categoryTitles.push(categoryList.pop());
   }
-
+  let currentRound = categoryTitles.reduce((acc, el) => {
+    data.clues.forEach((clue) => {
+      if(clue.categoryId === data.categories[el] ){
+        acc.push(clue);
+      }
+    })
+    return acc;
+  }, [])
+  return currentRound
+}
 }
 
 export default RoundChecker;
