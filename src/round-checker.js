fade07f78bdcb4edb3feb93c1eab4b0274de395f
Duy Vu <duyvu@Duys-MacBook-Pro.local>
2019-01-31 18:09:26 -0700

    round-checker-branch

--- src/round-checker.js
+++ src/round-checker.js
@@ -1,5 +1,19 @@
+import data from './data.js'
+
 class RoundChecker {
   constructor() {
-    this.categories = 
+    this.categories;
+    this.roundInfo = []; 
   }
+  createCategories() {
+   this.categories = Object.keys(data.categories)
+   for (let i = this.categories.length - 1; i > 0; i--) {
+       const j = Math.floor(Math.random() * (i + 1));
+       [this.categories[i], this.categories[j]] = [this.categories[j], this.categories[i]];  
+    }
+
+  }
+
 }
+
+export default RoundChecker;
