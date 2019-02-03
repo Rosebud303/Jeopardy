fd72ab7b5f2609d71364dc0b487a1e2530022e01
Sally Haefling <sallyhaefling@gmail.com>
2019-02-02 17:53:31 -0700

    Score populates under correct player name and refactor

--- src/game.js
+++ src/game.js
@@ -1,5 +1,6 @@
-import Player from './player.js'
-import Round from './round-checker.js'
+import Player from './player.js';
+import Round from './round-checker.js';
+import domUpdates from './domUpdates.js';
 
 class Game {
   constructor() {
@@ -9,13 +10,15 @@ class Game {
   }
 
   createPlayers(name1, name2, name3) {
-    const player1 = new Player(name1);
-    const player2 = new Player(name2);
-    const player3 = new Player(name3);
+    const player1 = new Player(name1, 76);
+    const player2 = new Player(name2, 15);
+    const player3 = new Player(name3, 66);
 
     if(this.players.length <= 3) {
       this.players.push(player1, player2, player3); 
     }
+    domUpdates.displayPlayersName();
+    domUpdates.displayPlayerScore();
   }
 
   createRounds() {
@@ -24,7 +27,7 @@ class Game {
     const round3 = new Round();
 
     if(this.round.length <= 3) {
-      this.round.push(round1, round2, round3)
+      this.round.push(round1, round2, round3);
     }
     // const round = new Round(this.categories, this.leftOverCategories);
     // this.round.push(round);
@@ -33,16 +36,10 @@ class Game {
   chooseWinner() {
   // The winner is determined by the player with the highest 
   //total score at the end of all rounds
-  if(game.players[0].score > game.players[1].score && 
-    game.players[0].score > game.players[2].score) {
-    console.log('player 1 is winner')
-  } else if (game.players[1].score > game.players[2].score && 
-    game.players[1].score > game.players[0].score) {  
-    console.log('Player 2 is the winner')  
-  } else {
-    console.log('player 3 is winner')
-  }
-  //Look at Math.max method to refactor this above
+  let winner = this.players.sort((a, b) => {
+    return a.score - b.score;
+  }).pop();
+  console.log(winner);
 }
 }
 
