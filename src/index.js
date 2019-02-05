3294c2f7041b2f6eeb385369e302a6110f983dc9
Duy Vu <duyvu@Duys-MacBook-Pro.local>
2019-02-05 14:58:28 -0700

    Add clues to cards and style fixed up CSS

--- src/index.js
+++ src/index.js
@@ -9,28 +9,25 @@ import Round from './round-checker.js';
 import Game from './game.js';
 import $ from 'jquery';
 import jQuery from 'jquery';
+
 window.jQuery = $;
-// window.$ = $;
-
-// console.log('This is the JavaScript entry file - your code begins here.');
-
 
 window.round = new Round();
 
 window.game = new Game();
 
+let $cluesCard = $('.clue-js');
+
 $('.start-game').on('click', function() {
-  // e.preventDefault();
-  domUpdates.startGame();
-  // domUpdates.clearBoard();
-  // game.beginGame();
+  domUpdates.startGame(game);
   $('input').hide('slow');
   $('button').hide('slow');
-  //maybe get this above to toggle to say 'quit game' instead
 });
 
-    $('.clue-js').on('click', function() {
-      domUpdates.displayQuestion(event)
-      domUpdates.clearBoard();
-    })
+$cluesCard.on('click', function() {
+  domUpdates.displayQuestion(round.roundClues[$cluesCard.index($(event.target))].question)
+  // $cluesCard.index($(event.target)).text('').css('background-color', 'black')
+  domUpdates.clearBoard();
+});
+
 
--- src/round-checker.js
+++ src/round-checker.js
@@ -7,43 +7,31 @@ class Round {
     this.roundCategories = [];
     this.roundClues = [];
     this.finalRound = [];
-    // this.leftOverCategories = [];
-    // this.createRandomCategories();
   }
 
   getFourCategoriesPerRound() {
-    //this is to get 4 categories per round to get
-    //our clues - each time it also changes because of
-    //the randomizer inside of game
-    //Everytime we start a new round, we want to invoke
-    //this function to create 4 more new cats
     for(var i = 0; i < 4; i++) {
       this.roundCategories.push(game.categories.pop());
     }
     domUpdates.displayCategories(this.roundCategories)
-    //need to put spies whenever using domUpdates for testing
   }
 
   getCluesToPopulate() {
-    //reinvoke this to create new clues each round
-    //this filters out the right clues for the category for each round
     let filterClues = this.roundCategories.map((category) => {
       return game.clues.reduce((acc, clue) => {
         if(data.categories[category] === clue.categoryId) {
           acc.push(clue)
         }
         return acc;
-      }, [])
+      }, []);
     })
-    //this randomizes the categories clues below
     filterClues.forEach(clue => {
       for (let i = clue.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [clue[i], clue[j]] = [clue[j], clue[i]];  
     }
     })
-    // console.log(filterClues)
-    this.filterCluesToDisplayOnDom(filterClues); //passes filterclues down to the next function
+    this.filterCluesToDisplayOnDom(filterClues); 
   }
 
   filterCluesToDisplayOnDom(cluesContainer) {
@@ -55,10 +43,7 @@ class Round {
     }, [])
     domUpdates.displayPointValues(this.roundClues);
   }
-  //create another function here that grabs one clue value at a time
-  //ie $100, $200, etc. maybe do another map with a filter inside of it
-  //filter out the first 100, then 1st 200, etc.
-
+  
   cluesCreator(clues) {
     let currentClues = clues.reduce((acc, el) => {
     data.clues.forEach((clue) => {
